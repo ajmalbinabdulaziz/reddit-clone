95337f206c86314c52cee6403945c41438483caf
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2022-05-25 17:28:03 +0530

    First Commit

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    //   <div></div>
+    <div className="relative h-10 w-20">
+        <Image src="https://links.papareact.com/fqy"
+        layout="fill"
+        />
+    </div>
+  )
+}
+
+export default Header
