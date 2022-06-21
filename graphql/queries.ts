5b1322b77117afc624b466235d5da94582b5caf0
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2022-06-21 16:10:36 +0530

    made [postId].tsx, new post page, video-3.45 mnts

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -77,3 +77,36 @@ export const GET_ALL_POSTS_BY_TOPIC = gql`
         }
     }
 `
+
+export const GET_POST_BY_POST_ID = gql`
+    query MyQuery($post_id: ID!){
+        getPostByPostId(post_id: $post_id) {
+            body
+            created_at
+            id
+            image
+            subreddit_id
+            title
+            username
+            subreddit {
+                created_at
+                id
+                topic
+            }
+            comments {
+                created_at
+                id
+                post_id
+                text
+                username
+            }
+            votes {
+                created_at
+                id
+                post_id
+                upvote
+                username
+            }
+        }
+    }
+`
